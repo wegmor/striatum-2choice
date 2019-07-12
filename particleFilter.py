32796ff3a068512a44fa6a93e332ac0f43ef16cb
Emil Wärnberg <emilwa@kth.se>
2019-07-12 13:50:19 +0200

    Tuned the parameters of the particle filter a bit more carefully. It should still be improved more, particularly making it a smoother instead of a filter and add some kind of auto-tuning of the parameters.

--- particleFilter.py
+++ particleFilter.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import tqdm
 
-def particleFilter(tracking, nParticles = 2000):
+#from IPython.core.debugger import set_trace
+
+def particleFilter(tracking, nParticles = 2000, flattening=0.0):
     '''
     Particle filter for calculating speeds. Currently uses the body and tailBase markers, but
     I intend to add the ear markers to this as well.
@@ -16,6 +18,8 @@ def particleFilter(tracking, nParticles = 2000):
                
     nParticles - The number of particles in the filter. More particles give better accuracy
                  but this should saturate fairly quickly.
+    flattening - Set to a very small number (<<1e-10) to make the prior more flat. Helps
+                 handling larger inaccuracies in the tracking at the expense of some accuracy.
                  
     Returns:
     A pandas dataframe with the estimated positions and speeds for each frame.
@@ -37,7 +41,9 @@ def particleFilter(tracking, nParticles = 2000):
 
         L = _likelihood(tracking.iloc[i], **state)
 
-        W = np.exp(L)
+        W = np.exp(L) + flattening
+        #if np.sum(W) < 1e-12:
+        #    set_trace()
         W /= np.sum(W)
         for k in ("x", "y", "speed", "bodyAngleSpeed", "elongation"):
             means[k].append(np.dot(state[k], W))
@@ -57,28 +63,31 @@ def _likelihood(position, x, y, speed, bodyAngle, bodyAngleSpeed, elongation): #
     Calculate the log likelihoods for all particles.
     '''
     #Body
-    L  = position.body.likelihood * -((x - position.body.x)**2) / 0.5
-    L += position.body.likelihood * -((y - position.body.y)**2) / 0.5
+    L  = position.body.likelihood * -((x - position.body.x)**2) / 1.0
+    L += position.body.likelihood * -((y - position.body.y)**2) / 1.0
     
     #Tailbase
     tailBaseX = x - elongation*np.cos(bodyAngle)
     tailBaseY = y - elongation*np.sin(bodyAngle)
-    L += position.tailBase.likelihood * -((position.tailBase.x - tailBaseX)**2) / 0.5
-    L += position.tailBase.likelihood * -((position.tailBase.y - tailBaseY)**2) / 0.5
+    L += position.tailBase.likelihood * -((position.tailBase.x - tailBaseX)**2) / 1.0
+    L += position.tailBase.likelihood * -((position.tailBase.y - tailBaseY)**2) / 1.0
+    
+    #Elongation
+    L += -((elongation - 3.0)**2) / 1.0
     
     return L
 
 def _step(x, y, speed, bodyAngle, bodyAngleSpeed, elongation): #headAngle
-    x += speed * np.cos(bodyAngle) + np.random.normal(0, 0.5, len(x))
-    y += speed * np.sin(bodyAngle) + np.random.normal(0, 0.5, len(y))
-    speed += np.random.normal(0, 0.5, len(speed))
-    bodyAngle += np.random.normal(bodyAngleSpeed, 0.1, len(bodyAngle)) + 2*np.pi
+    x += speed * np.cos(bodyAngle) + np.random.normal(0, 1.0, len(x))
+    y += speed * np.sin(bodyAngle) + np.random.normal(0, 1.0, len(y))
+    speed += np.random.normal(0, 1.0, len(speed))
+    bodyAngle += np.random.normal(bodyAngleSpeed, 0.2, len(bodyAngle)) + 2*np.pi
     #bodyAngle %= 2*np.pi
     #bodyAngle -= 2*np.pi
     bodyAngleSpeed *= 0.8
     bodyAngleSpeed += np.random.normal(0, 0.2, len(bodyAngleSpeed))
     #headAngle += np.random.normal(0, 0.5, len(bodyAngle)) + 2*np.pi
-    elongation += np.random.normal(0, 0.1, len(elongation))
+    elongation += np.random.normal(0, 0.5, len(elongation))
     #headAngle %= 2*np.pi
     #headAngle -= np.pi
 
@@ -88,3 +97,7 @@ def _resample(W):
     U = np.random.uniform(0,1.0/len(W)) + np.arange(len(W))/len(W)
     cumW = np.cumsum(W)
     return np.searchsorted(cumW, U)
+
+def _dfParticles(state, sl=slice(None,None)):
+    '''Convert the set or particles to a dataframe. Useful for debugging.'''
+    return pd.DataFrame(state).iloc[sl]
