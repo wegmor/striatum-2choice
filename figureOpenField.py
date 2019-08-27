a09a45a2838116adf30919dae3aea07f9c81dfb3
Emil Wärnberg <emilwa@kth.se>
2019-08-27 11:41:57 +0200

    Swapped the order of the panels in the open field figure.

--- figureOpenField.py
+++ figureOpenField.py
@@ -42,63 +42,7 @@ else:
 segmentedBehavior = segmentedBehavior.set_index("session")
 
 
-#Panel A
-cachedDataPath = cacheFolder / "openFieldDecodingWithIncreasingNumberOfNeurons.pkl"
-if cachedDataPath.is_file():
-    decodingData = pd.read_pickle(cachedDataPath)
-else:
-    decodingData = analysisOpenField.decodeWithIncreasingNumberOfNeurons(endoDataPath, segmentedBehavior)
-    decodingData.to_pickle(cachedDataPath)
-
-decodingData.insert(1, "genotype", decodingData.session.str.split("_").str[0])
-plt.sca(layout.axes["decodeWithIncreasingNumberOfNeurons"]["axis"])
-for strSess, df in decodingData.groupby("session"):
-    genotype = strSess.split("_")[0]
-    plt.plot(df.groupby("nNeurons").realAccuracy.mean(), color=style.getColor(genotype),
-             alpha=0.2, lw=.5)
-    plt.plot(df.groupby("nNeurons").shuffledAccuracy.mean(), color=style.getColor("shuffled"),
-             alpha=0.2, lw=.5)
-for genotype, df in decodingData.groupby("genotype"):
-    plt.plot(df.groupby("nNeurons").realAccuracy.mean(), color=style.getColor(genotype),
-             alpha=1.0)
-plt.plot(decodingData.groupby("nNeurons").shuffledAccuracy.mean(), color=style.getColor("shuffled"),
-         alpha=1.0)
-
-order = ("oprm1", "d1", "a2a")
-meanHandles = [mpl.lines.Line2D([], [], color=style.getColor(g)) for g in order]
-shuffleHandle = mpl.lines.Line2D([], [], color=style.getColor("shuffled"))
-plt.legend(meanHandles+[shuffleHandle], order+("shuffled",), loc=(0.45, 0.45), ncol=2)
-
-plt.ylim(0,1)
-plt.xlim(0,300)
-plt.xlabel("Number of neurons")
-plt.ylabel("Decoding accuracy (%)")
-plt.yticks(np.linspace(0,1,5), np.linspace(0,100,5,dtype=np.int64))
-sns.despine(ax=plt.gca())
-
-## Panel B
-cachedDataPath = cacheFolder / "openFieldDecodingConfusion.pkl"
-if cachedDataPath.is_file():
-    decodingData = pd.read_pickle(cachedDataPath)
-else:
-    decodingData = analysisOpenField.decodingConfusion(endoDataPath, segmentedBehavior)
-    decodingData.to_pickle(cachedDataPath)
-order = ["stationary", "running", "leftTurn", "rightTurn"]
-decodingData["genotype"] = decodingData.sess.str.split("_").str[0]
-for gt, data in decodingData.groupby("genotype"):
-    weightedData = data.set_index(["true", "predicted"]).eval("occurencies * nNeurons")
-    weightedData = weightedData.groupby(level=[0,1]).sum().unstack()
-    weightedData /= weightedData.sum(axis=1)[:, np.newaxis]
-    ax = layout.axes["confusionMatrix_{}".format(gt)]["axis"]
-    yticks = order if gt == "oprm1" else False
-    sns.heatmap(weightedData[order].reindex(order), ax=ax, vmin=0, vmax=1, annot=True, fmt=".0%", cmap=cmocean.cm.amp,
-                cbar=False, xticklabels=order, yticklabels=yticks, annot_kws={'fontsize': 4.5},
-                linewidths=mpl.rcParams["axes.linewidth"])
-    ax.set_xlabel("Predicted" if gt=="d1" else None)
-    ax.set_ylabel("Truth" if gt=="oprm1" else None)
-    ax.set_title(gt[0].upper() + gt[1:])
-
-## Panel C
+## Panel A
 cachedDataPath = cacheFolder / "openFieldTunings.pkl"
 if cachedDataPath.is_file():
     tuningData = pd.read_pickle(cachedDataPath)
@@ -119,7 +63,7 @@ total_count = (df.groupby(['genotype','animal','date','action'])
 sign_pct = sign_count / total_count
 sign_pct['noNeurons'] = total_count.signp
 
-#order = order # Use same order as previous panel
+order = ["stationary", "running", "leftTurn", "rightTurn"]
 
 # v x coords for actions
 a2x = dict(zip(order, np.arange(.5,12)))
@@ -154,7 +98,7 @@ for tuning in ('signp','signn'):
         
         sns.despine(ax=ax)
         
-## Panel D
+## Panel B
 tunings = tuningData.set_index(["genotype", "animal", "date", "neuron", "action"]).tuning
 
 cax = layout.axes['corr_colorbar']['axis']
@@ -171,6 +115,64 @@ for genotype in ("oprm1", "d1", "a2a"):
     ax.set_xlabel(None)
     ax.set_ylabel(None)
     ax.set_title({'d1':'D1','a2a':'A2A','oprm1':'Oprm1'}[genotype])
+
+
+#Panel C
+cachedDataPath = cacheFolder / "openFieldDecodingWithIncreasingNumberOfNeurons.pkl"
+if cachedDataPath.is_file():
+    decodingData = pd.read_pickle(cachedDataPath)
+else:
+    decodingData = analysisOpenField.decodeWithIncreasingNumberOfNeurons(endoDataPath, segmentedBehavior)
+    decodingData.to_pickle(cachedDataPath)
+
+decodingData.insert(1, "genotype", decodingData.session.str.split("_").str[0])
+plt.sca(layout.axes["decodeWithIncreasingNumberOfNeurons"]["axis"])
+for strSess, df in decodingData.groupby("session"):
+    genotype = strSess.split("_")[0]
+    plt.plot(df.groupby("nNeurons").realAccuracy.mean(), color=style.getColor(genotype),
+             alpha=0.2, lw=.5)
+    plt.plot(df.groupby("nNeurons").shuffledAccuracy.mean(), color=style.getColor("shuffled"),
+             alpha=0.2, lw=.5)
+for genotype, df in decodingData.groupby("genotype"):
+    plt.plot(df.groupby("nNeurons").realAccuracy.mean(), color=style.getColor(genotype),
+             alpha=1.0)
+plt.plot(decodingData.groupby("nNeurons").shuffledAccuracy.mean(), color=style.getColor("shuffled"),
+         alpha=1.0)
+
+order = ("oprm1", "d1", "a2a")
+meanHandles = [mpl.lines.Line2D([], [], color=style.getColor(g)) for g in order]
+shuffleHandle = mpl.lines.Line2D([], [], color=style.getColor("shuffled"))
+plt.legend(meanHandles+[shuffleHandle], order+("shuffled",), loc=(0.45, 0.45), ncol=2)
+
+plt.ylim(0,1)
+plt.xlim(0,300)
+plt.xlabel("Number of neurons")
+plt.ylabel("Decoding accuracy (%)")
+plt.yticks(np.linspace(0,1,5), np.linspace(0,100,5,dtype=np.int64))
+sns.despine(ax=plt.gca())
+
+## Panel D
+cachedDataPath = cacheFolder / "openFieldDecodingConfusion.pkl"
+if cachedDataPath.is_file():
+    decodingData = pd.read_pickle(cachedDataPath)
+else:
+    decodingData = analysisOpenField.decodingConfusion(endoDataPath, segmentedBehavior)
+    decodingData.to_pickle(cachedDataPath)
+order = ["stationary", "running", "leftTurn", "rightTurn"]
+decodingData["genotype"] = decodingData.sess.str.split("_").str[0]
+for gt, data in decodingData.groupby("genotype"):
+    weightedData = data.set_index(["true", "predicted"]).eval("occurencies * nNeurons")
+    weightedData = weightedData.groupby(level=[0,1]).sum().unstack()
+    weightedData /= weightedData.sum(axis=1)[:, np.newaxis]
+    ax = layout.axes["confusionMatrix_{}".format(gt)]["axis"]
+    yticks = order if gt == "oprm1" else False
+    sns.heatmap(weightedData[order].reindex(order), ax=ax, vmin=0, vmax=1, annot=True, fmt=".0%", cmap=cmocean.cm.amp,
+                cbar=False, xticklabels=order, yticklabels=yticks, annot_kws={'fontsize': 4.5},
+                linewidths=mpl.rcParams["axes.linewidth"])
+    ax.set_xlabel("Predicted" if gt=="d1" else None)
+    ax.set_ylabel("Truth" if gt=="oprm1" else None)
+    ax.set_title(gt[0].upper() + gt[1:])
+
 
 
 layout.insert_figures('target_layer_name')
