24b0d24326f6bcf2c2c24b1a5aefc5062b515b3d
Moritz Weglage <mweglage@fastmail.fm>
2019-09-04 15:14:20 +0200

    added shuffle bar, and center port movement decoding

--- figureDecodingSupp.py
+++ figureDecodingSupp.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import scipy.stats
 import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.patches as mpatches
 import h5py
 import pathlib
 import figurefirst
@@ -20,23 +21,27 @@ import analysisDecoding
 import style
 
 style.set_context()
+plt.ioff
 
-endoDataPath = "endoData_2019.hdf"
-alignmentDataPath = "alignment_190227.hdf"
+#%%
+dataFolder = pathlib.Path("data")
+endoDataPath = dataFolder / "endoData_2019.hdf"
+alignmentDataPath = dataFolder / "alignment_190227.hdf"
 
 outputFolder = pathlib.Path("svg")
 cacheFolder = pathlib.Path("cache")
-templateFolder = pathlib.Path(__file__).parent / "templates"
+templateFolder = pathlib.Path("templates")
 
 if not outputFolder.is_dir():
     outputFolder.mkdir()
 if not cacheFolder.is_dir():
     cacheFolder.mkdir()
 
+#%%
 layout = figurefirst.FigureLayout(templateFolder / "decodingSupp.svg")
 layout.make_mplfigures()
 
-## Panel A
+#%% Panel A
 cachedDataPath = cacheFolder / "decodeConfusion.pkl"
 if cachedDataPath.is_file():
     decodingData = pd.read_pickle(cachedDataPath)
@@ -46,45 +51,82 @@ else:
 order = ["mC2L-", "mC2R-", "mL2C-", "mR2C-", "pL2Cd", "pL2Co", "pL2Cr",
          "pC2L-", "pC2R-", "pR2Cd", "pR2Co", "pR2Cr"]
 decodingData["genotype"] = decodingData.sess.str.split("_").str[0]
+
+cax = layout.axes['dec_colorbar']['axis']
+cax.tick_params(axis='both', which='both',length=0)
+
 for gt, data in decodingData.groupby("genotype"):
-    weightedData = data.set_index(["true", "predicted"]).eval("occurencies * nNeurons")
+    weightedData = data.set_index(["true", "predicted"]).eval("occurences * nNeurons")
     weightedData = weightedData.groupby(level=[0,1]).sum().unstack()
     weightedData /= weightedData.sum(axis=1)[:, np.newaxis]
     ax = layout.axes["confusionMatrix_{}".format(gt)]["axis"]
-    sns.heatmap(weightedData[order].reindex(order), ax=ax, vmin=0, vmax=1, annot=True, fmt=".0%", cmap=cmocean.cm.amp,
-                cbar=False, xticklabels=False, yticklabels=False, annot_kws={'fontsize': 4.5},
+    sns.heatmap(weightedData[order].reindex(order), ax=ax, vmin=0, vmax=1, annot=True, fmt=".0%",
+                cmap=cmocean.cm.amp, xticklabels=False, yticklabels=False,
+                annot_kws={'fontsize': 4.5},  cbar=True, cbar_ax=cax,
+                cbar_kws={'orientation':'horizontal', 'ticks':()},
                 linewidths=matplotlib.rcParams["axes.linewidth"])
     ax.set_xlabel(None)
     ax.set_ylabel(None)
+    
+cax.text(-.025, .25, 0, ha='right', va='center', fontdict={'fontsize':6},
+         transform=cax.transAxes)
+cax.text(1.025, .25, 100, ha='left', va='center', fontdict={'fontsize':6},
+         transform=cax.transAxes)
+cax.text(.5, 1.1, 'accuracy (%)', ha='center', va='bottom', fontdict={'fontsize':6},
+         transform=cax.transAxes)
 
-## Panel B
+
+#%% Panel C
 def calcCorr(df):
     r = scipy.stats.pearsonr(df.true, df.predicted)[0]
     return pd.Series((r, df.nNeurons.iloc[0]), ("correlation", "nNeurons"))
-titles = {'mC2L': 'Center-to-left', 'mC2R': 'Center-to-right',
-          'mL2C': 'Left-to-center', 'mR2C': 'Right-to-center'}
-for label in ("mC2L", "mC2R", "mL2C", "mR2C"):
+
+#titles = {'mC2L': 'Center-to-left', 'mC2R': 'Center-to-right',
+#          'mL2C': 'Left-to-center', 'mR2C': 'Right-to-center'}
+for label in ("mC2L", "mC2R", "mL2C", "mR2C","pC2L","pC2R"):
     cachedDataPath = cacheFolder / "decodeMovementProgress_{}.pkl".format(label)
     if cachedDataPath.is_file():
         decodingMovementProgress = pd.read_pickle(cachedDataPath)
     else:
         decodingMovementProgress = analysisDecoding.decodeMovementProgress(endoDataPath, label=label+"-")
         decodingMovementProgress.to_pickle(cachedDataPath)
-    avgCorr = decodingMovementProgress.query("not shuffle").groupby("sess").apply(calcCorr)
+    avgCorr = decodingMovementProgress.groupby(["shuffle","sess"]).apply(calcCorr).reset_index("shuffle")
     avgCorr["genotype"] = avgCorr.index.str.split("_").str[0]
+    avgCorr.loc[avgCorr.shuffle, "genotype"] = "shuffled"
     avgCorr["animal"] = avgCorr.index.str.split("_").str[1]
+    avgCorr.loc[avgCorr.shuffle, "animal"] = avgCorr.loc[avgCorr.shuffle, "animal"] + "_r"
     avgCorr["date"] = avgCorr.index.str.split("_").str[2]
     avgCorr.sort_values(["genotype", "animal", "date"], ascending=False, inplace=True)
+    
     ax = layout.axes["movementProgressCorrelations_{}".format(label)]["axis"]
     sessionBarPlot.sessionBarPlot(avgCorr, yCol="correlation", weightCol="nNeurons",
-                                  ax=ax, colorFunc=style.getColor, weightScale=0.05)
-    if label=="mC2L":
-        ax.set_ylabel("Correlation\ntruth and decoded")
+                                  genotypeOrder=('d1','a2a','oprm1','shuffled'),
+                                  ax=ax, colorFunc=style.getColor, weightScale=0.035)
+    
+    ax.axhline(0, ls=':', alpha=.5, lw=matplotlib.rcParams['axes.linewidth'],
+               color='k', clip_on=False)
+    ax.set_xticks(())
+    ax.set_yticks((0,.5,1))
+    ax.set_yticks((.25,.75), minor=True)
+    if label=="mL2C":
+        ax.set_ylabel("true X predicted\ncorrelation")
     else:
         ax.set_yticklabels([])
-    ax.set_title(titles[label])
+
     ax.set_ylim(0,1)
-    sns.despine(ax=ax)
+    sns.despine(ax=ax, bottom=True)
     
-layout.insert_figures('target_layer_name')
+axt = layout.axes['bar_legend']['axis']
+legend_elements = [mpatches.Patch(color=style.getColor(gt), alpha=.3,
+                                 label={'oprm1':'Oprm1', 'a2a':'A2A', 'd1':'D1',
+                                        'shuffled':'shuffled'}[gt])
+                   for gt in ['d1','a2a','oprm1','shuffled']
+                  ]
+axt.legend(handles=legend_elements, ncol=4, loc='center',
+           mode='expand')
+axt.axis('off')
+
+
+#%%
+layout.insert_figures('plots')
 layout.write_svg(outputFolder / "decodingSupp.svg")
--- utils/sessionBarPlot.py
+++ utils/sessionBarPlot.py
@@ -11,23 +11,25 @@ def bootstrapSEM(values, weights, iterations=1000):
 
 def sessionBarPlot(df, yCol, ax, colorFunc, genotypeCol="genotype", animalCol="animal",
                    dateCol="date", weightCol="noNeurons", weightScale=0.1,
-                   genotypeOrder=("oprm1", "d1", "a2a"), barAlpha=0.3):
+                   genotypeOrder=("d1","a2a","oprm1"), barAlpha=0.3):
 
     genotypeMeans = df.groupby(genotypeCol).apply(lambda df: np.average(df[yCol], weights=df[weightCol]))
     genotypeSEMs = df.groupby(genotypeCol).apply(lambda df: bootstrapSEM(df[yCol], weights=df[weightCol]))
     genotypeMeans = genotypeMeans.loc[list(genotypeOrder)]
     genotypeSEMs = genotypeSEMs.loc[list(genotypeOrder)]
     barColors = [colorFunc(gt) for gt in genotypeMeans.index]
-    ax.bar((0,1,2), genotypeMeans, 0.9, color=barColors, alpha=barAlpha, yerr=genotypeSEMs)
+    ax.bar(np.arange(len(genotypeOrder)), genotypeMeans, 0.9, color=barColors,
+           alpha=barAlpha, yerr=genotypeSEMs, clip_on=False)
 
     perAnimal = df.sort_values(dateCol).drop_duplicates(animalCol)
     cumCount = perAnimal.groupby(genotypeCol).cumcount()
     nAnimalsPerGenotype = perAnimal.groupby(genotypeCol).size().reindex(perAnimal[genotypeCol])
-    genotypeIndex = pd.Series((0,1,2), genotypeOrder).reindex(perAnimal[genotypeCol])
+    genotypeIndex = pd.Series(np.arange(len(genotypeOrder)), genotypeOrder).reindex(perAnimal[genotypeCol])
     xCoord = (cumCount.values / (nAnimalsPerGenotype.values-1))*0.6 + 0.2 - 0.5 + genotypeIndex.values
     xCoord = pd.Series(xCoord, perAnimal[animalCol]).reindex(df[animalCol])
     xCoord.index = df.index
     c = [colorFunc(gt) for gt in df[genotypeCol]]
-    ax.scatter(xCoord, df[yCol], df[weightCol]*weightScale, edgecolors=c, facecolor="none")
-    ax.set_xticks((0,1,2))
+    ax.scatter(xCoord, df[yCol], df[weightCol]*weightScale, edgecolors=c, facecolor="none",
+               clip_on=False, lw=.5)
+    ax.set_xticks(np.arange(len(genotypeOrder)))
     ax.set_xticklabels(genotypeOrder)
