5dbb66d606c9317eee4b09c9e1275cc18657f69d
Emil Wärnberg <emilwa@kth.se>
2021-01-06 19:21:29 +0100

    Started working on a new version for figure 4.

--- figure4FollowNeurons.py
+++ figure4FollowNeurons.py
@@ -7,11 +7,16 @@ import h5py
 import pathlib
 import figurefirst
 import cmocean
+import pims
+import tqdm
+import skimage
+import itertools
 from utils import readSessions, fancyViz, alluvialPlot
 import analysisOpenField
 import analysisTunings
 import analysisDecoding
 import analysisOftVs2Choice
+import analysisKinematicsSupp
 import style
 #import subprocess
 style.set_context()
@@ -22,6 +27,7 @@ endoDataPath = pathlib.Path('data') / "endoData_2019.hdf"
 alignmentDataPath = pathlib.Path('data') / "alignment_190227.hdf"
 outputFolder = pathlib.Path('svg')
 templateFolder = pathlib.Path('templates')
+videoFolder = pathlib.Path('/home/emil/2choice/openFieldVideos') #TODO: make this relative
 
 if not outputFolder.is_dir():
     outputFolder.mkdir()
@@ -33,8 +39,8 @@ layout.make_mplfigures()
 
 #%%
 genotypeNames = {'d1':'D1','a2a':'A2A','oprm1':'Oprm1'}
-behaviorNames = {'stationary': 'stationary', 'running': 'running', 'leftTurn': 'left turn',
-                 'rightTurn': 'right turn'}
+behaviorNames = {'stationary': 'stationary', 'running': 'running', 'leftTurn':
+                 'left turn', 'rightTurn': 'right turn'}
 
 def bootstrapSEM(values, weights, iterations=1000):
     avgs = []
@@ -43,13 +49,179 @@ def bootstrapSEM(values, weights, iterations=1000):
         avgs.append(np.average(values.iloc[idx], weights=weights.iloc[idx]))
     return np.std(avgs)
 
-
-#%% remapping example neurons plot
 ofSess =  next(readSessions.findSessions(endoDataPath, task='openField',
                                          animal='5308', date='190201'))
 chSess = next(readSessions.findSessions(endoDataPath, task='2choice',
                                         animal=ofSess.meta.animal,
                                         date=ofSess.meta.date))
+
+#%%
+open_field_video = pims.open(str(videoFolder / ofSess.meta.video) + ".avi")
+tracking = ofSess.readTracking()
+segmented = pd.read_pickle("cache/segmentedBehavior.pkl").loc[str(ofSess)]
+background = np.median([open_field_video.get_frame(i) for i in tqdm.trange(2000)], axis=0)
+
+videoFolder = pathlib.Path('/mnt/dmclab/striatum_2choice/') #Again, this should be a relative path
+two_choice_video = pims.open(str(videoFolder / chSess.meta.video) + ".avi")
+cmap = sns.cubehelix_palette(start=1.4, rot=.8*np.pi, light=.75, as_cmap=True)
+
+#%% get example oft turn
+ex_action = (0,80)
+start, stop = segmented.loc[ex_action][["startFrame", "stopFrame"]]
+frame_ids = list(range(start, stop+1, 5))
+frames = np.array([open_field_video.get_frame(i) for i in frame_ids])
+coords = tracking.loc[start:stop]
+
+
+#%% plot first oft frame
+ax = layout.axes['trajectoryIllustration','openField']['axis']
+
+ax.imshow(skimage.exposure.adjust_log(frames[0],1.3))
+xy = coords.iloc[0]
+ax.plot([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+        [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+        color='yellow', lw=mpl.rcParams['axes.linewidth'], zorder=1)
+ax.scatter([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+           [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+           color='yellow', zorder=1, marker='.')
+ax.set_xlim((120,709))
+ax.set_ylim((590,0))
+ax.fill([120, 300, 120, 120], [0, 0, 80, 0], color=cmap(0.0), alpha=.6)
+ax.text(120+10, 10, "+0.00s", fontsize=6, va="top",
+        color="w")#bbox=dict(facecolor=cmap(0.0), alpha=1.0))
+wallCorners = ofSess.getWallCorners()
+cm2px = (wallCorners.lowerRight.x - wallCorners.lowerLeft.x)/49
+ax.plot([709-5*cm2px, 709], [600, 600], 'k', clip_on=False)
+ax.axis('off')
+
+for i in range(1, 6):
+    ax = layout.axes['trajectoryIllustration','openField_sub{}'.format(i)]['axis']
+    ax.imshow(skimage.exposure.adjust_log(frames[i],1.3))
+    xy = coords.iloc[i*5]
+    ax.plot([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+            [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+            color='yellow', lw=mpl.rcParams['axes.linewidth']/5, zorder=1)
+    ax.scatter([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+               [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+               color='yellow', s=.2, zorder=1, marker='.')
+    ax.set_xlim((120,709))
+    ax.set_ylim((590,0))
+    ax.axhspan(0, 190, color=cmap(i*5/(stop-start)), alpha=.6)
+    ax.text((120+709)/2, 50, "+{:.2f}s".format(i*5/20.0), fontsize=6, va="top",
+            ha="center", color="w")#))
+    ax.axis('off')
+    
+#%% plot example oft turn trajectory
+ax = layout.axes['trajectoryIllustration','turnTrajectory']['axis']
+
+diff = (frames - background).mean(axis=-1)
+alpha = ((np.clip(diff, -75, -40) + 40) / -35)
+dx = coords.iloc[0].body.x - coords.iloc[0].tailBase.x
+dy = coords.iloc[0].body.y - coords.iloc[0].tailBase.y
+rot = np.rad2deg(np.arctan2(dy, dx))+90
+t = ax.transData
+tr = mpl.transforms.Affine2D().rotate_deg_around(coords.iloc[0].body.x,
+                                                 coords.iloc[0].body.y, -rot) + t
+
+for i,xy in coords.iterrows():
+    ax.plot([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+            [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+            color='yellow', transform=tr, zorder=1, lw=mpl.rcParams['axes.linewidth'])
+
+xlims = ax.get_xlim()
+ylims = ax.get_ylim()
+
+
+plt.sca(ax)
+for i in range(len(frames)):
+    frame = frames[i].mean(axis=-1) + i*255 - 255*len(frames)/2# + 256
+    fancyViz.imshowWithAlpha(frame, .9*alpha[i], 255*len(frames)/2, cmap=cmap,
+                             transform=tr, interpolation='antialiased')
+
+ax.hlines(coords.iloc[0].body.y, coords.iloc[0].body.x-60, coords.iloc[0].body.x+60,
+          ls=':', lw=mpl.rcParams['axes.linewidth'], color='k', alpha=.5, zorder=2,
+          clip_on=False)
+ax.vlines(coords.iloc[0].body.x, coords.iloc[0].body.y-60, coords.iloc[0].body.y+60,
+          ls=':', lw=mpl.rcParams['axes.linewidth'], color='k', alpha=.5, zorder=2,
+          clip_on=False)
+
+ax.set_xlim(np.array(xlims) + [-40,40])
+ax.set_ylim(np.array(ylims)[::-1] + [20,-40])
+ax.axis('off')
+#sat = 255*len(frames)/2
+#mpl.colors.Normalize(-sat, sat)
+cax = layout.axes['trajectoryIllustration','turnTrajectory_colorbar']['axis']
+cb = plt.colorbar(mpl.cm.ScalarMappable(None, cmap), cax=cax, orientation='horizontal')
+cb.outline.set_visible(False)
+cax.set_axis_off()
+for t in (0, 0.5, 1.0):
+    text = '+{:.2f}s\n({:.0f}%)'.format(t*(stop-start)/20.0, t*100)
+    cax.text(t, -0.5, text, ha='center', va='top', fontdict={'fontsize':6})
+cax.text(0.5, -3.5, 'time (progess)', ha='center', va="top", fontdict={'fontsize':6})
+
+#%% plot 2 choice frame
+chTracking = chSess.readTracking()
+n = 3933
+frame = two_choice_video.get_frame(n)
+
+ax = layout.axes['trajectoryIllustration','choice']['axis']
+ax.set_ylim((750,0))
+ax.set_xlim((55,800))
+t = ax.transData
+tr = mpl.transforms.Affine2D().rotate_deg_around(frame.shape[1]/2, frame.shape[0]/2, -90) + t
+
+ax.imshow(skimage.exposure.adjust_gamma(frame, .7), transform=tr)
+xy = chTracking.loc[n]
+ax.plot([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+        [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+        color='yellow', lw=mpl.rcParams['axes.linewidth'], zorder=1,
+        transform=tr)
+ax.scatter([xy.tailBase.x, xy.body.x, 0.5*(xy.leftEar.x + xy.rightEar.x)],
+           [xy.tailBase.y, xy.body.y, 0.5*(xy.leftEar.y + xy.rightEar.y)],
+           color='yellow', zorder=1, marker='.', transform=tr)
+wallCorners = chSess.getWallCorners()
+cm2px = (wallCorners.lowerRight.x - wallCorners.lowerLeft.x)/15
+ax.plot([800-5*cm2px, 800], [770, 770], 'k', clip_on=False)
+ax.axis('off')
+
+#%%
+ofSegs = analysisKinematicsSupp.openFieldSegmentKinematics(endoDataPath)
+tcSegs = analysisKinematicsSupp.twoChoiceSegmentKinematics(endoDataPath)
+fps = 20
+#Change from per frame to per second
+tcSegs['bodyAngleSpeed'] *= -180/np.pi * fps
+tcSegs['speed'] *= fps
+ofSegs['bodyAngleSpeed'] *= -180/np.pi * fps
+ofSegs['speed'] *= fps
+kinematicParams = ["bodyAngleSpeed", "speed", "elongation"]
+
+phases = ['mC2L','mC2R','mL2C','mR2C','pC2L','pC2R','pL2C','pR2C', 'dL2C', 'dR2C']
+behaviors = ['leftTurn', 'rightTurn', 'running', 'stationary']
+lims = {'speed': (-10, 30), 'bodyAngleSpeed': (-150, 150),
+        'elongation': (2.25, 3.75)}
+xlabels = {'speed': 'speed (cm/s)',
+           'bodyAngleSpeed': 'turning speed (deg/s)',
+           'elongation': 'elongation (cm)'}
+for k in ["bodyAngleSpeed"]:#kinematicParams:
+    ax = layout.axes['kinematicHist_twoChoice_'+k]['axis']
+    for p in phases:
+        sns.kdeplot(tcSegs[tcSegs.label==p][k], cut=0, ax=ax,
+                    color=style.getColor(p), alpha=.7, legend=False)
+    ax.set_xlim(*lims[k])
+    ax.set_yticks([])
+    ax.set_xticklabels([])
+    sns.despine(ax=ax)
+    ax = layout.axes['kinematicHist_openField_'+k]['axis']
+    for b in behaviors:
+        sns.kdeplot(ofSegs[ofSegs.label==b][k], cut=0, ax=ax,
+                    color=style.getColor(b), alpha=.7, legend=False)
+    ax.set_xlim(*lims[k])
+    ax.set_yticks([])
+    ax.set_xlabel(xlabels[k])
+    sns.despine(ax=ax)
+
+
+#%% remapping example neurons plot
 ofTraces = ofSess.readDeconvolvedTraces(rScore=True).reset_index(drop=True)
 chTraces = chSess.readDeconvolvedTraces(rScore=True).reset_index(drop=True)
 ofTracking = analysisOftVs2Choice.getSmoothedTracking(endoDataPath, ofSess.meta.genotype, 
@@ -59,7 +231,7 @@ chTracking = analysisOftVs2Choice.getSmoothedTracking(endoDataPath, ofSess.meta.
                                                       ofSess.meta.animal, ofSess.meta.date,
                                                       '2choice')
 
-for n,neuron in enumerate([86,192][::-1]):
+for n,neuron in [(1,86)]:#enumerate([86,192][::-1]):
     ofTrace = ofTraces[neuron]
     chTrace = chTraces[neuron]
 
@@ -138,6 +310,58 @@ cax.text(-1.05, -.3, '-1', ha='right', va='center', fontdict={'fontsize':6})
 cax.text(1.05, -.3, '1', ha='left', va='center', fontdict={'fontsize':6})
 cax.text(0, 1.1, 'z-score', ha='center', va='bottom', fontdict={'fontsize':6})
 
+#%%
+openFieldTunings = analysisOpenField.getTuningData(endoDataPath)
+twoChoiceTunings = analysisTunings.getTuningData(endoDataPath)
+for t in (twoChoiceTunings, openFieldTunings):
+    t['signp'] = t['pct'] > .995
+    t.set_index(["animal", "date", "action", "neuron"], inplace=True)
+    t.sort_index(inplace=True)
+
+labels = ["leftTurn"]#, "mL2C", "mR2C", "all", "running", "mC2L", "mC2R"
+saturation = 0.3
+fvs = {}
+for key in itertools.product(('d1', 'a2a', 'oprm1'), labels):
+    fvs[("2choice",)+key] = fancyViz.SchematicIntensityPlot(splitReturns=False,
+                                linewidth=mpl.rcParams['axes.linewidth'],
+                                smoothing=5, saturation=saturation)
+    
+for key in itertools.product(('d1', 'a2a', 'oprm1'), labels):
+    fvs[("openField",)+key] = fancyViz.OpenFieldSchematicPlot(
+                                linewidth=mpl.rcParams['axes.linewidth'],
+                                smoothing=3, saturation=saturation)
+
+for sess in readSessions.findSessions(endoDataPath, task=["2choice", "openField"]):
+    shortKey = (sess.meta.animal, sess.meta.date)
+    if shortKey not in openFieldTunings.index: continue
+    if shortKey not in twoChoiceTunings.index: continue
+    traces = sess.readDeconvolvedTraces(zScore=True)
+    genotype = sess.meta.genotype
+    task = sess.meta.task
+    for label in labels:
+        fvs[(task, genotype, label)].setSession(sess)
+        if label == "all": #Don't look at tuning
+            tuned = np.full(traces.shape[1], True)
+        elif label[0] == 'm': #2-choice tuning
+            tuned = twoChoiceTunings.loc[shortKey+(label+"-",)].signp
+        else: #Open field tuning
+            tuned = openFieldTunings.loc[shortKey+(label,)].signp
+        for neuron in traces.columns:
+            if tuned[neuron]:
+                fvs[(task, genotype, label)].addTraceToBuffer(traces[neuron])
+for task, gt, label in itertools.product(("openField", "2choice"),
+                                         ("d1", "a2a", "oprm1"), labels):
+    axName = "_".join(("mean", label, "of" if task=="openField" else task, gt))
+    img = fvs[(task, gt, label)].drawBuffer(ax=layout.axes[axName]['axis'])
+
+for i in range(1,2):#4):
+    cax = layout.axes['colorbar_means_{}'.format(i)]['axis']
+    cb = plt.colorbar(img, cax=cax, orientation='horizontal')
+    cb.outline.set_visible(False)
+    cax.set_axis_off()
+    cax.text(-0.325, -.1, "{:.1f}".format(-saturation), ha='right', va='center', fontdict={'fontsize':6})
+    cax.text(0.325, -.1, "{:.1f}".format(saturation), ha='left', va='center', fontdict={'fontsize':6})
+    cax.text(0, 0.5, 'z-score', ha='center', va='bottom', fontdict={'fontsize':6})
 
 #%% oft vs choice left/right tuning comparison
 ofTuningData = analysisOpenField.getTuningData(endoDataPath)
@@ -299,6 +523,119 @@ print("alluvial (followed neurons):")
 print(primaryPairs.groupby("genotype").size())
 
 
+#%%
+kinematics_of_all = analysisKinematicsSupp.openFieldSegmentKinematics(endoDataPath)
+kinematics_tc_all = analysisKinematicsSupp.twoChoiceSegmentKinematics(endoDataPath)
+fps = 20
+#Change from per frame to per second
+kinematics_of_all['bodyAngleSpeed'] *= -180/np.pi * fps
+kinematics_of_all['speed'] *= fps
+kinematics_tc_all['bodyAngleSpeed'] *= -180/np.pi * fps
+kinematics_tc_all['speed'] *= fps
+
+ax = layout.axes['kinematics3d_openField']['axis']
+ax.view_init(60, -70)#(30, -60)
+ax.scatter(kinematics_of_all.bodyAngleSpeed,
+           kinematics_of_all.speed,
+           kinematics_of_all.elongation, s=1,
+           c=[style.getColor(l) for l in kinematics_of_all.label],
+           alpha=.2, lw=0)
+ax.set_xlim(-120, 120)
+ax.set_ylim(-7.5, 25)
+ax.set_zlim(2.5, 4)
+ax.set_title("open field")
+
+ax = layout.axes['kinematics3d_2choice']['axis']
+ax.view_init(60, -70)#(30, -60)
+validPhases = ["mC2L", "mC2R", "mL2C", "mR2C",
+               "pC2L", "pC2R", "dL2C", "dR2C",
+               "pL2C", "pR2C"]
+mask = kinematics_tc_all.label.isin(validPhases)
+ax.scatter(kinematics_tc_all.bodyAngleSpeed[mask],
+           kinematics_tc_all.speed[mask],
+           kinematics_tc_all.elongation[mask], s=1,
+           c=[style.getColor(l) for l in kinematics_tc_all.label[mask]],
+           alpha=.2, lw=0)
+ax.set_xlim(-120, 120)
+ax.set_ylim(-5, 15)
+ax.set_zlim(2.5, 3.4)
+ax.set_title("2-choice")
+for ax in (layout.axes['kinematics3d_openField']['axis'],
+           layout.axes['kinematics3d_2choice']['axis']):
+    ax.set_xlabel("turning speed (deg/s)")
+    ax.set_ylabel("speed (cm/s)")
+    ax.set_zlabel("elongation (cm)")
+
+#%%
+nNeurons = []
+for sess in analysisKinematicsSupp.find2choiceSessionsFollowingOpenField(endoDataPath):
+    deconv = sess.readDeconvolvedTraces(zScore=False)
+    nNeurons.append((str(sess), deconv.shape[1]))
+nNeurons = pd.DataFrame(nNeurons, columns=["sess", "nNeurons"])
+nNeurons.set_index("sess", inplace=True)
+
+twoChoicePdists = analysisKinematicsSupp.twoChoicePdists(endoDataPath)
+openFieldPdists = analysisKinematicsSupp.openFieldPdists(endoDataPath)
+cdists = analysisKinematicsSupp.openFieldToTwoChoiceCdists(endoDataPath)
+dist_list = [twoChoicePdists, openFieldPdists, cdists]
+cols = ["C3", "C4", "C5"]
+gts = ["d1", "a2a", "oprm1"]
+for i, dist in enumerate(dist_list):
+    mean = {gt: np.zeros(20-1) for gt in gts}
+    nTot = {gt: 0 for gt in gts}
+    nPairs = {gt: np.zeros(20-1) for gt in gts}
+    for s, g in dist.groupby(level=0):
+        bins = pd.cut(g.kinematics_dist, np.linspace(.1, 4, 20))
+        binned = g.groupby(bins).mean()
+        gt = s.split("_")[0]
+        n = nNeurons.loc[s].nNeurons
+        ax = layout.axes['kinematicsVsDeconv_'+gt]['axis']
+        ax.plot(binned.kinematics_dist, binned.deconv_dist, color=cols[i],
+                alpha=np.clip(n/500, 0.1, 1.0), lw=.5)
+        mean[gt] += n*binned.deconv_dist
+        nPairs[gt] += g.groupby(bins).size()
+        nTot[gt] += n
+    for gt in gts:
+        mean[gt] /= nTot[gt]
+        ax = layout.axes['kinematicsVsDeconv_'+gt]['axis']
+        ax.plot(binned.kinematics_dist, mean[gt], color=cols[i], lw=2)
+        ax = layout.axes['kinematicsPairHist_'+gt]['axis']
+        nPairs[gt] /= nPairs[gt].sum()
+        ax.plot(binned.kinematics_dist, nPairs[gt]*4, color=cols[i])
+        
+gt_names = {"d1": "D1+", "a2a": "A2A+", "oprm1": "Oprm1+"}
+for gt in gts:
+    ax = layout.axes['kinematicsVsDeconv_'+gt]['axis']
+    ax.set_xlim(0, 4)
+    ax.set_ylim(-0.05, 0.12)
+    ax.axhline(0, color="k", alpha=0.3, lw=0.5, linestyle="--")
+    ax.set_title(gt_names[gt], color=style.getColor(gt))
+    ax.set_yticks(np.arange(-0.05, 0.15, 0.05))
+    ax.set_xticklabels([])
+    if gt=="d1":
+        ax.set_ylabel("ensamble correlation")
+    else:
+        ax.set_yticklabels([])
+    if gt=="oprm1":
+        lines = [mpl.lines.Line2D([], [], color=c, label=l) 
+                 for c,l in zip(cols, ["open field → open field",
+                                       "2-choice → 2-choice",
+                                       "open field → 2-choice"])]
+        ax.legend(handles=lines, loc=(1.05, 0.4))#'center right')
+    sns.despine(ax=ax)
+    
+    ax = layout.axes['kinematicsPairHist_'+gt]['axis']
+    ax.set_xlim(0, 4)
+    ax.set_ylim(0, 0.5)
+    if gt=="d1":
+        ax.set_ylabel("pdf")
+    else:
+        ax.set_yticklabels([])
+    if gt=="a2a":
+        ax.set_xlabel("kinematic dissimilarity (Mahalanobis distance)")
+    sns.despine(ax=ax)
+
+'''
 #%% Panel C
 sel_neurons = [92,44,16]
 sel_colors = list(cmocean.cm.phase(i) for i in [.25,.5,.75])
@@ -466,6 +803,8 @@ axt.legend(handles=legend_elements, loc=(-0.7, 1.08), ncol=2)
 
 print("Panel E:")
 print(selection.groupby([pd.cut(selection.dayDifference, (1, 4, 14, 100)), "genotype"]).size())
+'''
+
 #%%
 layout.insert_figures('plots')
 layout.write_svg(outputFolder / svgName)
