ff445de40dd93b6135390e7be711e4f6cc47ee03
Emil Wärnberg <emilwa@kth.se>
2021-01-08 17:24:55 +0100

    Moved the 2-choice across days panels from figure 4 (commented out) to figure 3.

--- figure3Tunings.py
+++ figure3Tunings.py
@@ -17,6 +17,8 @@ from matplotlib.ticker import MultipleLocator, FixedLocator
 import scipy.stats
 from utils import readSessions, fancyViz, roiPlot #, alluvialPlot
 from collections import defaultdict
+import cmocean
+import h5py
 import pathlib
 import figurefirst
 import style
@@ -30,6 +32,7 @@ plt.ioff()
 style.set_context()
 
 endoDataPath = pathlib.Path("data") / "endoData_2019.hdf"
+alignmentDataPath = pathlib.Path('data') / "alignment_190227.hdf"
 outputFolder = pathlib.Path("svg")
 templateFolder = pathlib.Path("templates")
 
@@ -40,6 +43,14 @@ if not outputFolder.is_dir():
 svgName = 'figure3Tunings.svg'
 layout = figurefirst.FigureLayout(templateFolder / svgName)
 layout.make_mplfigures()
+
+#%%
+def bootstrapSEM(values, weights, iterations=1000):
+    avgs = []
+    for _ in range(iterations):
+        idx = np.random.choice(len(values), len(values), replace=True)
+        avgs.append(np.average(values.iloc[idx], weights=weights.iloc[idx]))
+    return np.std(avgs)
 
 #%%
 tuningData = analysisTunings.getTuningData(endoDataPath)
@@ -602,6 +613,175 @@ ax.legend(handles=legend_elements, loc='center')
 ax.axis('off')
 print("Panel N (sessions):")
 print(avgCorr.groupby(level=0).size())
+
+
+#%% Panel O
+sel_neurons = [92,44,16]
+sel_colors = list(cmocean.cm.phase(i) for i in [.25,.5,.75])
+examples = [('oprm1','5703',('190114','190116','190126'), neuron)
+                for neuron in sel_neurons]
+
+d1_rois = pd.read_hdf(endoDataPath, key='/rois/{}/{}/{}'.format(examples[0][0],
+                                                                examples[0][1],
+                                                                examples[0][2][0]))
+d1_rois = np.array([d1_rois[n].unstack('x').values for n in d1_rois])
+
+alignments= h5py.File(alignmentDataPath, 'r')
+alignment_path = 'data/{}/{}/{}/{}'.format(examples[0][0],
+                                           examples[0][1],
+                                           examples[0][2][0],
+                                           examples[0][2][2])
+d2_rois = alignments[alignment_path + '/A_t'].value
+match = alignments[alignment_path + '/match'].value
+alignments.close()
+
+colors = np.array([list(cmocean.cm.phase(i))[:-1] for i in np.random.rand(len(match))])
+colors[np.isin(match[:,0], sel_neurons)] = np.array(sel_colors)[:,:-1]
+for d, drois in enumerate((d1_rois, d2_rois)):
+    rois_bg = drois 
+    rois = drois[match[:,d]]
+    sel_cnts = analysisDecoding.get_centers(rois_bg[{0:sel_neurons,
+                                                     1:[dict(match)[n] for n in sel_neurons]}[d]])
+    
+    rs = []
+    for roi, color in zip(rois, colors):
+        roi /= roi.max()
+        roi = roi**1.5
+        roi = np.clip(roi-.1, 0, .85)
+        roi /= roi.max()
+        r = np.array([(roi > 0).astype('int')]*3) * color[:, np.newaxis, np.newaxis]
+        r = np.concatenate([r, roi[np.newaxis]], axis=0)
+        rs.append(r.transpose((1,2,0)))    
+    rs = np.array(rs)
+    
+    rs_bg = []
+    for roi in rois_bg:
+        roi /= roi.max()
+        roi = roi**1.5
+        roi = np.clip(roi-.1, 0, .85)
+        roi /= roi.max()
+        rs_bg.append(roi)
+    img_bg = np.array(rs_bg).max(axis=0)
+
+    ax = layout.axes['alignment_example_d{}'.format(d+1)]['axis']
+    base = ax.transData
+    rot = mpl.transforms.Affine2D().rotate_deg(-45)
+    ax.imshow(img_bg, cmap='bone_r', vmin=0, vmax=1, alpha=.5, aspect='auto', transform=rot+base)
+    ax.axis("equal")
+    ax.set_xlim((100, 325))
+    ax.set_ylim((160, -170))
+    for img in rs:
+        ax.imshow(img, aspect='auto', transform=rot+base)
+    ax.scatter(sel_cnts[:,0], sel_cnts[:,1], marker='o', edgecolor='k',
+               facecolor='none', s=10, alpha=1, lw=mpl.rcParams['axes.linewidth'],
+               transform=rot+base)
+    ax.axis('off')
+
+
+##%% Panel P
+alignmentStore = h5py.File(alignmentDataPath, "r")
+def findAlignedNeuron(genotype, animal, fromDate, toDate, neuron):
+    if fromDate == toDate:
+        return neuron
+    else:
+        matches = alignmentStore["/data/{}/{}/{}/{}/match".format(genotype, animal, fromDate, toDate)]
+        return pd.Series(matches[:,1], matches[:,0]).loc[neuron]
+
+saturation = 1
+for i in range(3):
+    for j in range(3):
+        sess = next(readSessions.findSessions(endoDataPath, animal=examples[i][1],
+                                             date=examples[i][2][j], task="2choice"))
+        neuron = findAlignedNeuron(examples[i][0], examples[i][1], examples[i][2][0],
+                                   examples[i][2][j], examples[i][3])
+        signal = sess.readDeconvolvedTraces()[neuron]
+        signal -= signal.mean()
+        signal /= signal.std()
+        ax = layout.axes["acrossDays_ex{}{}".format(i+1,j+1)]["axis"]
+        fv = fancyViz.SchematicIntensityPlot(sess, splitReturns=False,
+                                             linewidth=mpl.rcParams['axes.linewidth'],
+                                             saturation=saturation, smoothing=7)
+        img = fv.draw(signal, ax=ax)
+    
+    axbg = layout.axes['acrossDays_ex{}1_bg'.format(i+1)]['axis']
+    axbg.axvspan(-.055, -.03, .1, .93, color=sel_colors[i], alpha=1,
+                 clip_on=False)
+    axbg.set_xlim((0,1))
+    axbg.set_axis_off()
+
+cax = layout.axes['colorbar']['axis']
+cb = plt.colorbar(img, cax=cax, orientation='horizontal')
+cb.outline.set_visible(False)
+cax.set_axis_off()
+cax.text(-.025, .25, -saturation, ha='right', va='center', fontdict={'fontsize':6},
+         transform=cax.transAxes)
+cax.text(1.025, .25, saturation, ha='left', va='center', fontdict={'fontsize':6},
+         transform=cax.transAxes)
+cax.text(.5, 1.1, 'z-score', ha='center', va='bottom', fontdict={'fontsize':6},
+         transform=cax.transAxes)
+
+
+#%% Panel Q
+decodingAcrossDays = analysisDecoding.decodingAcrossDays(endoDataPath, alignmentDataPath)
+
+fromDate = pd.to_datetime(decodingAcrossDays.fromDate, format="%y%m%d")
+toDate = pd.to_datetime(decodingAcrossDays.toDate, format="%y%m%d")
+td = (toDate - fromDate).dt.days
+decodingAcrossDays["dayDifference"] = td
+selection = decodingAcrossDays.query("fromTask=='2choice' & toTask=='2choice'")
+
+for i,l,h in ((0,1,3), (1,4,13), (2,14,100)):
+    g = selection.query("dayDifference >= {} & dayDifference <= {}".format(l,h)).groupby(["animal", "fromDate", "toDate"])
+    
+    perAnimal = g.mean()[['nNeurons', 'sameDayScore', 'nextDayScore', 'sameDayShuffled', 'nextDayShuffled']]
+    perAnimal["genotype"] = g.genotype.first()
+    
+    scaledScore = perAnimal[['sameDayScore', 'nextDayScore']] * perAnimal.nNeurons[:,np.newaxis]
+    perGenotype = scaledScore.groupby(perAnimal.genotype).sum()
+    perGenotype /= perAnimal.groupby("genotype").nNeurons.sum()[:, np.newaxis]
+    
+    shuffleScore = perAnimal[['sameDayShuffled', 'nextDayShuffled']] * perAnimal.nNeurons[:,np.newaxis]
+    shuffleScore = shuffleScore.sum(axis=0) / perAnimal.nNeurons.sum()
+    
+    plt.sca(layout.axes["decodingAcrossDays_{}".format(i+1)]["axis"])
+    
+    for r in perAnimal.itertuples():
+        plt.plot([0,1], [r.sameDayScore, r.nextDayScore], lw=style.lw()*r.nNeurons/400.0,
+                 c=style.getColor(r.genotype), alpha=0.2)
+    for r in perGenotype.itertuples():
+        gt = r.Index
+        animalsWithGt = perAnimal.query("genotype == '{}'".format(gt))
+        sameDaySEM = bootstrapSEM(animalsWithGt.sameDayScore, animalsWithGt.nNeurons)
+        nextDaySEM = bootstrapSEM(animalsWithGt.nextDayScore, animalsWithGt.nNeurons)
+        plt.errorbar([0,1], [r.sameDayScore, r.nextDayScore], [sameDaySEM, nextDaySEM],
+                     lw=style.lw(), c=style.getColor(gt))
+    
+    sameDayShuffledSEM = bootstrapSEM(perAnimal.sameDayShuffled, perAnimal.nNeurons)
+    nextDayShuffledSEM = bootstrapSEM(perAnimal.nextDayShuffled, perAnimal.nNeurons)
+    plt.errorbar([0,1], [shuffleScore.sameDayShuffled, shuffleScore.nextDayShuffled],
+                 [sameDayShuffledSEM, nextDayShuffledSEM],
+                 lw=style.lw(), c=style.getColor("shuffled"))
+    
+    plt.ylim(0,1)
+    plt.xlim(-0.25, 1.25)
+    #xlab = ("1-3 days later", "4-13 days later", "14+ days later")
+    #plt.xticks((0,1), ("same day", xlab[i]), rotation=90)
+    plt.xticks([])
+    plt.xlabel(("1-3", "4-13", "14+")[i] + "\ndays", labelpad=7, fontsize=6)
+    if i==0:
+        plt.yticks(np.linspace(0,1,5), np.linspace(0,100,5,dtype=np.int64))
+        plt.ylabel("decoding accuracy (%)")
+    else:
+        plt.yticks([])#np.linspace(0,1,5), [""]*5)
+    plt.axhline(0, color='k', lw=0.5, alpha=0.5, ls=":")
+    sns.despine(ax=plt.gca(), left=(i!=0), bottom=True)
+axt = layout.axes['decodingAcrossDays_2']['axis']
+genotypeNames["shuffled"] = "shuffled"
+legend_elements = [mpl.lines.Line2D([0], [0], color=style.getColor(g), label=genotypeNames[g]) for g in ("d1", "a2a", "oprm1", "shuffled")]
+axt.legend(handles=legend_elements, loc=(-0.7, 1.08), ncol=2)
+
+print("Panel Q:")
+print(selection.groupby([pd.cut(selection.dayDifference, (1, 4, 14, 100)), "genotype"]).size())
 
 #%%
 layout.insert_figures('plots')
